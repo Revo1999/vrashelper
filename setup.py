bd87ed53ac3070de5a159745512354dfd679f0b5
Revo1999 <victor.rassers@gmail.com>
2024-04-13 14:18:14 +0200

    Uploading package v0.1

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vrashelper',
+    version='0.1',
+    packages=find_packages(),
+    author='Victor Rasmussen',
+    description='Primarely Quality of life stuff for console text',
+    url='https://github.com/Revo1999/vrashelper',
+)
